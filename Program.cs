d78aa7f8eda0ce9ce0dca92e500894102563771a
vitor20022003 <vitorm4123@gmail.com>
2020-11-20 00:06:36 -0300

    base de arrays

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alimentos = { "feijão ", "arroz ", "bife ", "frango ", "carne moida ", "macarrão ", "frango " };
+
+            for (int i = 0; i < alimentos.Length; i++)
+            {
+                Console.WriteLine(alimentos[i]);
+            }
+
+        }
+    }
+}
